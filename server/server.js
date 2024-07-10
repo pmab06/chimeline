db2d0c006b7d7675f94d3c66acc44eb97e8773b3
pmab06 <navybluue.1@gmail.com>
2024-07-10 13:41:47 -0300

    Implemented Express

--- server/server.js
+++ server/server.js
@@ -1,8 +1,24 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
+const port = 3000;
 
-app.get("/api", (req,res)=>{
-	res.json({"users":["NavyBluue_","navy240sx","stNavy"]})
-})
+// Middleware para permitir requests cross-origin durante o desenvolvimento
+const cors = require('cors');
+app.use(cors());
 
-app.listen(5000, () => {console.log("Hi! Server started on port 5000")})
+// Serve arquivos estáticos do diretório build do React
+app.use(express.static(path.join(__dirname, '../client/src')));
+
+app.get('/api', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+// Para todas as outras rotas, serve o arquivo index.html do React
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Hi! Express server running on port ${port}`);
+});
